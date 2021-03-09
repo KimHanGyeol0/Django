5b981b96b2b25ea6b8d8eda6b3f139976165d13b
gyeol <gksruf1128@naver.com>
2021-03-09 17:31:39 +0900

    Intro Django pjt

--- 00_django_inro_p2/pages/urls.py
+++ 00_django_inro_p2/pages/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('answer/', views.answer, name='answer'),
     path('lotto/', views.lotto, name='lotto'),
     path('dinner/<str:menu>/<int:people>/', views.dinner, name='dinner'),
+    path('lotto2/', views.lotto2, name='lotto2'),
+    path('lotto_automatic/', views.lotto_automatic, name='lotto_automatic'),
+    path('lotto_manual/', views.lotto_manual, name='lotto_manual'),
+    path('lotto_buy/', views.lotto_buy, name='lotto_buy'),
 ]
 
 # {% url pages:index %}
--- 00_django_inro_p2/pages/views.py
+++ 00_django_inro_p2/pages/views.py
@@ -39,7 +39,7 @@ def answer(request):
     return render(request, 'pages/answer.html', context)
 
 def lotto(request):
-    times = random.randint(1, 500)
+    times = random.randint(1, 950)
     number = sorted(random.sample(range(1, 46), 6))
     url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={times}'
     res = requests.get(url)
@@ -69,3 +69,45 @@ def dinner(request, menu, people):
         'people': people,
     }
     return render(request, 'pages/dinner.html', context)
+
+def lotto2(request):
+    return render(request, 'pages/lotto2.html')
+
+def lotto_automatic(request):
+    number = sorted(random.sample(range(1, 46), 6))
+    context = {
+        'number': number,
+    }
+    return render(request, 'pages/lotto_automatic.html', context)
+
+def lotto_manual(request):
+    return render(request, 'pages/lotto_manual.html')
+
+def lotto_buy(request):
+    number = []
+    for i in range(1, 7):
+        number.append(request.GET.get(f'number{i}'))
+    number.sort()
+    time = request.GET.get('time')
+    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={time}'
+    res = requests.get(url)
+    lotto_info = res.json()
+    lotto_numbers = []
+    answer = []
+    count = 0
+    for i in range(1, 7):
+        lotto_num = lotto_info.get(f'drwtNo{i}')
+        lotto_numbers.append(lotto_num)
+        if lotto_num in number:
+            answer.append(lotto_num)
+            count += 1
+    lotto_numbers.sort()
+    
+    context = {
+        'time': time,
+        'number': number,
+        'answer': answer,
+        'count': count,
+        'lotto_numbers': lotto_numbers,
+    }
+    return render(request, 'pages/lotto_buy.html', context)
